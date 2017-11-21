83af457eb9d8c5da76ef0727c36bf805dcf2064d
brian Yeh <brian@brians-MacBook-Pro.local>
2017-11-21 00:08:32 -0800

    made setup idempotent

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
+git clone https://github.com/tomasr/molokai.git ~/.vim/colors
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-mv vimrc ~/.vimrc
+cp vimrc ~/.vimrc
 vim +PluginInstall +qall
-mv tmux.conf ~/.tmux.conf
+cp tmux.conf ~/.tmux.conf
