0fe56c0d0776cbb5028be5c16847d7f04446a9a9
brian Yeh <brian@brians-MacBook-Pro.local>
2017-11-21 01:08:04 -0800

    added same dir pane in tmux conf

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ git clone https://github.com/tomasr/molokai.git ~/temp
 mkdir ~/.vim/colors
 mv ~/temp/colors/molokai.vim ~/.vim/colors/molokai.vim
 sudo rm -rd ~/temp
+
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp vimrc ~/.vimrc
 vim +PluginInstall +qall
