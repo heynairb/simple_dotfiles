bbda332b53ebb20fb0e9ce46237c6c5a292f6736
brian yeh <brian@brians-MacBook-Pro.local>
2017-12-04 08:46:37 -0800

    move color installation to after plugin installation

--- setup.sh
+++ setup.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/tomasr/molokai.git ~/temp
-mkdir ~/.vim/colors
-mv ~/temp/colors/molokai.vim ~/.vim/colors/molokai.vim
-sudo rm -rd ~/temp
-
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp vimrc ~/.vimrc
 vim +PlugInstall +qall
 ~/.vim/plugged/YouCompleteMe/install.py --all
 cp tmux.conf ~/.tmux.conf
+
+git clone https://github.com/tomasr/molokai.git ~/temp
+mkdir ~/.vim/colors
+mv ~/temp/colors/molokai.vim ~/.vim/colors/molokai.vim
+sudo rm -rd ~/temp
+
