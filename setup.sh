7643f5056137b77e0ebcea08a21af5f857c798f6
Brian Yeh <byeh@nepfin.com>
2018-03-30 09:09:09 -0700

    added airline theme to tmux

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ cp vimrc ~/.vimrc
 vim +PlugInstall +qall
 ~/.vim/plugged/YouCompleteMe/install.py --all
 cp tmux.conf ~/.tmux.conf
+cp tmux-airline.conf ~/.tmux-airline.conf
 cp bash_profile ~/.bash_profile
 
 
@@ -18,7 +19,7 @@ git clone git@github.com:powerline/fonts.git ~/temp/fonts
 ~/temp/fonts/install.sh
 
 git clone git@github.com:sgolovine/PlexNerdfont.git ~/temp/plexfonts
-cp ~/temp/plexfonts/Regular/. ~/Library/Fonts
+cp -r ~/temp/plexfonts/Regular/ ~/Library/Fonts
 
 sudo rm -rd ~/temp
 
